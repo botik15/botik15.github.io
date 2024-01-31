42615347aee9d4234edd3d2d730b94259e31e25c
Слава Васильев <36413274+botik15@users.noreply.github.com>
2024-01-31 13:29:20 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ import asyncio
 import logging
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command
+from datetime import datetime
 
 # Включаем логирование, чтобы не пропустить важные сообщения
 logging.basicConfig(level=logging.INFO)
