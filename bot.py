a1a15ad15452943e33b4b3e522386183371eb3aa
Слава Васильев <36413274+botik15@users.noreply.github.com>
2024-01-31 14:08:32 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -133,33 +133,67 @@ async def cmd_advanced_example(message: Message):
 
 
 
-@dp.message(Command("settimer"))
-async def cmd_settimer(
-        message: Message,
-        command: CommandObject
-):
-    # Если не переданы никакие аргументы, то
-    # command.args будет None
-    if command.args is None:
-        await message.answer(
-            "Ошибка: не переданы аргументы"
+
+
+
+
+
+
+
+from aiogram.types import FSInputFile, URLInputFile, BufferedInputFile
+
+@dp.message(Command('images'))
+async def upload_photo(message: Message):
+    # Сюда будем помещать file_id отправленных файлов, чтобы потом ими воспользоваться
+    file_ids = []
+
+    # Чтобы продемонстрировать BufferedInputFile, воспользуемся "классическим"
+    # открытием файла через `open()`. Но, вообще говоря, этот способ
+    # лучше всего подходит для отправки байтов из оперативной памяти
+    # после проведения каких-либо манипуляций, например, редактированием через Pillow
+    with open("buffer_emulation.jpg", "rb") as image_from_buffer:
+        result = await message.answer_photo(
+            BufferedInputFile(
+                image_from_buffer.read(),
+                filename="image from buffer.jpg"
+            ),
+            caption="Изображение из буфера"
         )
-        return
-    # Пробуем разделить аргументы на две части по первому встречному пробелу
-    try:
-        delay_time, text_to_send = command.args.split(" ", maxsplit=1)
-    # Если получилось меньше двух частей, вылетит ValueError
-    except ValueError:
-        await message.answer(
-            "Ошибка: неправильный формат команды. Пример:\n"
-            "/settimer <time> <message>"
-        )
-        return
-    await message.answer(
-        "Таймер добавлен!\n"
-        f"Время: {delay_time}\n"
-        f"Текст: {text_to_send}"
+        file_ids.append(result.photo[-1].file_id)
+
+    # Отправка файла из файловой системы
+    image_from_pc = FSInputFile("image_from_pc.jpg")
+    result = await message.answer_photo(
+        image_from_pc,
+        caption="Изображение из файла на компьютере"
     )
+    file_ids.append(result.photo[-1].file_id)
+
+    # Отправка файла по ссылке
+    image_from_url = URLInputFile("https://picsum.photos/seed/groosha/400/300")
+    result = await message.answer_photo(
+        image_from_url,
+        caption="Изображение по ссылке"
+    )
+    file_ids.append(result.photo[-1].file_id)
+    await message.answer("Отправленные файлы:\n"+"\n".join(file_ids))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # Запуск процесса поллинга новых апдейтов
 async def main():
