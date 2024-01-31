7e8d2d9e39c40f722860529cd05e2e56728890fc
Слава Васильев <36413274+botik15@users.noreply.github.com>
2024-01-31 14:18:49 +0300

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,35 @@
+import asyncio
+import logging
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters.command import Command
+from datetime import datetime
+from config_reader import config
+from aiogram import F, html
+from aiogram.types import Message  
+
+
+# Включаем логирование, чтобы не пропустить важные сообщения
+logging.basicConfig(level=logging.INFO)
+# Объект бота
+bot = Bot(token=config.bot_token.get_secret_value(), parse_mode="HTML")
+# Диспетчер
+dp = Dispatcher()
+ 
+ @dp.message(Command("start"))
+async def cmd_start(message: types.Message):
+    kb = [
+        [types.KeyboardButton(text="С пюрешкой")],
+        [types.KeyboardButton(text="Без пюрешки")]
+    ]
+    keyboard = types.ReplyKeyboardMarkup(keyboard=kb)
+    await message.answer("Как подавать котлеты?", reply_markup=keyboard)
+
+
+
+
+# Запуск процесса поллинга новых апдейтов
+async def main():
+    await dp.start_polling(bot,mylist=[1, 2, 3])
+
+if __name__ == "__main__":
+    asyncio.run(main())
