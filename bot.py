7ac9a2dfefa85d5ede96b22dda80f8c8a5818844
Слава Васильев <36413274+botik15@users.noreply.github.com>
2024-01-31 14:21:49 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -38,11 +38,11 @@ async def cmd_start(message: types.Message):
 
 @dp.message(F.text.lower() == "с пюрешкой")
 async def with_puree(message: types.Message):
-    await message.reply("Отличный выбор!")
+    await message.reply("Отличный выбор!", reply_markup=types.ReplyKeyboardRemove())
 
 @dp.message(F.text.lower() == "без пюрешки")
 async def without_puree(message: types.Message):
-    await message.reply("Так невкусно!")
+    await message.reply("Так невкусно!", reply_markup=types.ReplyKeyboardRemove())
 
 
 # Запуск процесса поллинга новых апдейтов
