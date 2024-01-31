9fd1445afdf09989a6c52987381e5d9a82b29fc4
Слава Васильев <36413274+botik15@users.noreply.github.com>
2024-01-31 13:11:57 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,7 +14,16 @@ dp = Dispatcher()
 @dp.message(Command("start"))
 async def cmd_start(message: types.Message):
     await message.answer("Hello!")
+    
+# Хэндлер на команду /test1
+@dp.message(Command("test1"))
+async def cmd_test1(message: types.Message):
+    await message.reply("Test 1")
 
+# Хэндлер на команду /test2
+async def cmd_test2(message: types.Message):
+    await message.reply("Test 2")
+    
 # Запуск процесса поллинга новых апдейтов
 async def main():
     await dp.start_polling(bot)
