8b97ac646d1ac50c0dae00c6e532aac9e9c7db46
Слава Васильев <36413274+botik15@users.noreply.github.com>
2024-01-31 14:26:09 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -44,20 +44,61 @@ dp = Dispatcher()
 
 
 
-# новый импорт!
-from aiogram.utils.keyboard import ReplyKeyboardBuilder
+# # новый импорт!
+# from aiogram.utils.keyboard import ReplyKeyboardBuilder
 
-@dp.message(Command("reply_builder"))
-async def reply_builder(message: types.Message):
+# @dp.message(Command("reply_builder"))
+# async def reply_builder(message: types.Message):
+#     builder = ReplyKeyboardBuilder()
+#     for i in range(1, 17):
+#         builder.add(types.KeyboardButton(text=str(i)))
+#     builder.adjust(4)
+#     await message.answer(
+#         "Выберите число:",
+#         reply_markup=builder.as_markup(resize_keyboard=True),
+#     )
+
+
+
+@dp.message(Command("special_buttons"))
+async def cmd_special_buttons(message: types.Message):
     builder = ReplyKeyboardBuilder()
-    for i in range(1, 17):
-        builder.add(types.KeyboardButton(text=str(i)))
-    builder.adjust(4)
+    # метод row позволяет явным образом сформировать ряд
+    # из одной или нескольких кнопок. Например, первый ряд
+    # будет состоять из двух кнопок...
+    builder.row(
+        types.KeyboardButton(text="Запросить геолокацию", request_location=True),
+        types.KeyboardButton(text="Запросить контакт", request_contact=True)
+    )
+    # ... второй из одной ...
+    builder.row(types.KeyboardButton(
+        text="Создать викторину",
+        request_poll=types.KeyboardButtonPollType(type="quiz"))
+    )
+    # ... а третий снова из двух
+    builder.row(
+        types.KeyboardButton(
+            text="Выбрать премиум пользователя",
+            request_user=types.KeyboardButtonRequestUser(
+                request_id=1,
+                user_is_premium=True
+            )
+        ),
+        types.KeyboardButton(
+            text="Выбрать супергруппу с форумами",
+            request_chat=types.KeyboardButtonRequestChat(
+                request_id=2,
+                chat_is_channel=False,
+                chat_is_forum=True
+            )
+        )
+    )
+    # WebApp-ов пока нет, сорри :(
+
     await message.answer(
-        "Выберите число:",
+        "Выберите действие:",
         reply_markup=builder.as_markup(resize_keyboard=True),
     )
-
 
 # Запуск процесса поллинга новых апдейтов
 async def main():
