c0f79c236a08bf0f6b9bfdf9589f290b7a542d58
Слава Васильев <36413274+botik15@users.noreply.github.com>
2024-01-31 14:11:25 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -139,51 +139,12 @@ async def cmd_advanced_example(message: Message):
 
 
 
-
-from aiogram.types import FSInputFile, URLInputFile, BufferedInputFile
-
-@dp.message(Command('images'))
-async def upload_photo(message: Message):
-    # Сюда будем помещать file_id отправленных файлов, чтобы потом ими воспользоваться
-    file_ids = []
-
-    # Чтобы продемонстрировать BufferedInputFile, воспользуемся "классическим"
-    # открытием файла через `open()`. Но, вообще говоря, этот способ
-    # лучше всего подходит для отправки байтов из оперативной памяти
-    # после проведения каких-либо манипуляций, например, редактированием через Pillow
-    with open("buffer_emulation.jpg", "rb") as image_from_buffer:
-        result = await message.answer_photo(
-            BufferedInputFile(
-                image_from_buffer.read(),
-                filename="image from buffer.jpg"
-            ),
-            caption="Изображение из буфера"
-        )
-        file_ids.append(result.photo[-1].file_id)
-
-    # Отправка файла из файловой системы
-    image_from_pc = FSInputFile("image_from_pc.jpg")
-    result = await message.answer_photo(
-        image_from_pc,
-        caption="Изображение из файла на компьютере"
+@dp.message(F.photo)
+async def download_photo(message: Message, bot: Bot):
+    await bot.download(
+        message.photo[-1],
+        destination=f"/tmp/{message.photo[-1].file_id}.jpg"
     )
-    file_ids.append(result.photo[-1].file_id)
-
-    # Отправка файла по ссылке
-    image_from_url = URLInputFile("https://picsum.photos/seed/groosha/400/300")
-    result = await message.answer_photo(
-        image_from_url,
-        caption="Изображение по ссылке"
-    )
-    file_ids.append(result.photo[-1].file_id)
-    await message.answer("Отправленные файлы:\n"+"\n".join(file_ids))
-
-
-
-
-
-
-
 
 
 
