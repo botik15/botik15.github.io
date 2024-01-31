069faaa28e37a945da4cdc15f3d078001e65f536
Слава Васильев <36413274+botik15@users.noreply.github.com>
2024-01-31 13:47:05 +0300

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,34 @@
+import asyncio
+import logging
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters.command import Command
+from datetime import datetime
+from config_reader import config
+
+# Включаем логирование, чтобы не пропустить важные сообщения
+logging.basicConfig(level=logging.INFO)
+# Объект бота
+bot = Bot(token=config.bot_token.get_secret_value())
+# Диспетчер
+dp = Dispatcher()
+
+
+# Если не указать фильтр F.text, 
+# то хэндлер сработает даже на картинку с подписью /test
+@dp.message(F.text, Command("test"))
+async def any_message(message: Message):
+    await message.answer(
+        "Hello, <b>world</b>!", 
+        parse_mode=ParseMode.HTML
+    )
+    await message.answer(
+        "Hello, *world*\!", 
+        parse_mode=ParseMode.MARKDOWN_V2
+    )
+
+# Запуск процесса поллинга новых апдейтов
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
