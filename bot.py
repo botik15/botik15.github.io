9ff48eb72c45987730cd856139e09da82c91f118
Слава Васильев <36413274+botik15@users.noreply.github.com>
2024-02-01 09:09:48 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -31,8 +31,7 @@ async def cmd_hello(message: Message):
     await message.answer(output)
 
 # Запуск процесса поллинга новых апдейтов
-async def main(): 
-    await bot.delete_webhook(drop_pending_updates=True)
+async def main():  
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
