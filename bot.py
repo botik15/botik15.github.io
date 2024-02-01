ed1fc436b76a497cf1fc431f4e6e75e1704c4d57
Слава Васильев <36413274+botik15@users.noreply.github.com>
2024-02-01 09:17:32 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,6 +6,7 @@ from datetime import datetime
 from config_reader import config
 from aiogram import F, html
 from aiogram.types import Message  
+from aiogram.types import InputFile
 
 
 # Включаем логирование, чтобы не пропустить важные сообщения
@@ -20,16 +21,13 @@ import os
 from subprocess import run, STDOUT, PIPE
 # указывайте полный путь к запускаемой 
 # программе/команде или она не будет работать
-
-
-output = run(cmd.split(), stdout=PIPE, stderr=STDOUT, text=True) 
+ 
 
 @dp.message(Command("hello"))
 async def cmd_hello(message: Message):
-    cmd = 'ls'
-    # перенаправляем `stdout` и `stderr` в переменную `output`
+    path = 'git_dowland_py.sh'
+    await message.answer_document(InputFile(path))
     
-    await message.answer(output)
 
 # Запуск процесса поллинга новых апдейтов
 async def main():   
